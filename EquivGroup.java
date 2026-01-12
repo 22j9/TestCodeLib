9d740444fb56cbb992614a717442aaf0366f7da4
22j9 <zzjing.uw@gmail.com>
2026-01-12 01:30:03 -0800

    Update EquivGroup.java

--- EquivGroup.java
+++ EquivGroup.java
@@ -1,37 +1,37 @@
 public class Solution {
+    // Time O(N*G) -> O(n^2) Space O(n)
     public List<List<Integer>> partitionArray(List<Integer> nums, BiPredicate<Integer, Integer> equiv) {
         if (nums.isEmpty()) {
             return new ArrayList<>();
         }
-
         // Use a list to maintain the order of groups as they are discovered.\
         // [G0, G1, G2...]
         List<List<Integer>> equivGroups = new ArrayList<>();
         // Store a representative element for each group to check for equivalence.
         // [repG0, repG1, repG2...]
         List<Integer> representatives = new ArrayList<>();
-
+        // above are in the same order -> implicit mapping
         for (int num : nums) {
-            boolean foundGroup = false;
+            boolean foundItsGroup = false;
             // [G0, G1, G2, G3...]
+            // check with every rep from existing groups
             for (int i = 0; i < representatives.size(); i++) {
                 int rep = representatives.get(i);
                 if (equiv.test(num, rep)) {
                     equivGroups.get(i).add(num);
-                    foundGroup = true;
+                    foundItsGroup = true;
                     break;
                 }
             }
             // if it does not belong to any existing equiv groups
-            // it by itself is an equiv group
-            if (!foundGroup) {
+            // it by itself is an equiv group, and it's its first rep
+            if (!foundItsGroup) {
                 List<Integer> newGroup = new ArrayList<>();
                 newGroup.add(num);
                 equivGroups.add(newGroup);
                 representatives.add(num);
             }
         }
-
         return equivGroups;
     }
 }
